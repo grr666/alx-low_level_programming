6dacfc9e7e091ef21d39e39a097d91ecee6bbbff
aman <girmaamanuel5@gmail.com>
2022-12-19 03:11:30 -0800

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,32 +2,27 @@
 
 /**
   * rev_string - Reverses a string
-  * @s: The string to be modified
+  * @s:npsrameter
   *
-  * Return: void
   */
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
+	"sam" -> "mas"
+	char tmp;
+	int i, length1, length2;
 
-	a = s;
+	length1 = 0;
+	length2 = 0;
 
-	while (s[c] != '\0')
+	while (s[length1] != '\0')
+		length1++;
+
+	lengrh2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
 	{
-		c++;
-	}
-
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 = 1;
 	}
 }
