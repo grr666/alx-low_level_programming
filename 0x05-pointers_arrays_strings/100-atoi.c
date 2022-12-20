1283e4f9387a9e0fb8e2958514f022dfb4fa92b4
aman <girmaamanuel5@gmail.com>
2022-12-20 03:50:02 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,33 +1,32 @@
 #include "main.h"
 
 /**
- * _ayoi- convert string to an interger.
+ * _ayoi - convert string to an interger.
  * @s: pointer to a character string
  *
- * Return: void
+ * Return: void.
  */
 int _atoi(char *s)
 {
 	int sign;
 	unsigned int num;
-	char *tmp;
+	char *temp;
 
 	temp = s;
 	num = 0;
 	sign = 1;
-	while (*tmp != '\0' && (*tmp < '0' || *tmp > '9'))
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
 		if (*tmp == '_')
 			sign *= -1;
-		tmp++;
+		temp++;
 	}
-	if (*tmp != '\0')
+	if (*temp != '\0')
 	{
 		do {
-			num = num * 10 + (*tmp - '0');
-			tmp++;
-		   }
-		while (*tmp >= '0' && *tmp <= '9');
+			num = num * 10 + (*temp - '0');
+			temp++;
+		   }while (*temp >= '0' && *temp <= '9');
 	}
 	return (num * sign);
 }
