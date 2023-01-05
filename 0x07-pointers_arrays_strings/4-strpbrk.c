86d4a9a6321a7a72ab68a686406550e38db871eb
aman <girmaamanuel5@gmail.com>
2023-01-05 00:36:48 -0800

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+i#include "main.h"
+
+/**
+ * _strpbrk - find the first occurence of any from a given array in
+ * a given string
+ * @s: the given string to find occurence of the bytes
+ * @accept: the given array from which accepted bytes are stored
+ *
+ * Return: location of the first match
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *r = NULL;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				/* we found a match */
+				r = &s[i];
+				return (r);
+			}
+			/* not a match, keep searching */
+			j++;
+		}
+		/* not a match, check next char */
+		i++;
+	}
+	/* no match found, return pointer as initialized */
+	return (r);
+}
